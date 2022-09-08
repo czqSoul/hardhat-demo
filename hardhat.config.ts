aab5c615cca439910e0b171443dc9fd6d42eb045
Soul <soul.chen@coinflex.com>
2022-09-08 02:46:51 +0000

    First commit

---
+++ hardhat.config.ts
@@ -0,0 +1,41 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const INFURA_ID = process.env.INFURA_ID;
+const ETHERSCAN_ID = process.env.ETHERSCAN_ID;
+const config: HardhatUserConfig = {
+  defaultNetwork: "goerli",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_ID}`,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 5,
+    },
+    // localhost: {
+    //   url: "http://127.0.0.1:8545/",
+    //   // saveDeployments: true,
+    //   chainId: 31337,
+    // },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_ID,
+  },
+};
+
+export default config;
